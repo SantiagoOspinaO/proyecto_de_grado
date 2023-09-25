acb62f6f94b2788846ef760bdaa7ec15215084ba
Juan Ceballos <juanestebanceballos@gmail.com>
2023-09-24 19:01:54 -0500

    Se agrega la seguridad, pero se desacriva para que se pueda seguir el desarrollo

---
+++ src/main/java/co/com/crud/requirement/persistence/security/constant/SecurityConstants.java
@@ -0,0 +1,9 @@
+package co.com.crud.requirement.persistence.security.constant;
+
+public class SecurityConstants {
+    private SecurityConstants() {
+    }
+
+    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public static final String JWT_HEADER = "Authorization";
+}

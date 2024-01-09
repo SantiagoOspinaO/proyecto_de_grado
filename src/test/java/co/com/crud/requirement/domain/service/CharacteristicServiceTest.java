ff298a295166c7536ccc840061d4b61b036cffc9
Manuel Echverri <manuelecheverrir@yahoo.com>
2024-01-08 23:39:47 -0500

    Unit test add

--- src/test/java/co/com/crud/requirement/domain/service/CharacteristicServiceTest.java
+++ src/test/java/co/com/crud/requirement/domain/service/CharacteristicServiceTest.java
@@ -1,8 +1,11 @@
 package co.com.crud.requirement.domain.service;
 
 import co.com.crud.requirement.domain.model.Characteristic;
+import co.com.crud.requirement.domain.model.Requirement;
+import co.com.crud.requirement.domain.model.queryresult.*;
 import co.com.crud.requirement.domain.repository.CharacteristicDomainRepository;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -40,6 +43,136 @@ public class CharacteristicServiceTest {
         verify(mockCharacteristicRepository, times(1)).getAllCharacteristics();
         assertNotNull(result);
     }
+
+    @Test
+    public void testGetCharacteristicByRequirementId(){
+        //Arrange
+        CharacteristicDomainRepository mockCharacteristicRepository = mock(CharacteristicDomainRepository.class);
+        Requirement requirement = new Requirement();
+        requirement.setRequirementId(1);
+        requirement.setProjectId(1);
+        requirement.setName("Requisito ensayo");
+        requirement.setDescription("Requisito ensayo prueba");
+        requirement.setTypeRequirement("Funcional");
+
+        List<ICharacteristicsByRequirementId> mockICharacteristics = new ArrayList<ICharacteristicsByRequirementId>();
+        when(mockCharacteristicRepository.getCharacteristicsByRequirementId(requirement.getRequirementId())).thenReturn(mockICharacteristics);
+        CharacteristicService characteristicService = new CharacteristicService(mockCharacteristicRepository);
+        //Act
+        List<ICharacteristicsByRequirementId> result = characteristicService.getCharacteristicByRequirement(requirement.getRequirementId());
+        //Assert
+        Mockito.verify(mockCharacteristicRepository, Mockito.times(1)).getCharacteristicsByRequirementId(requirement.getRequirementId());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCountRequirementsByTypeAndNameCharacteristic(){
+        //Arrange
+        CharacteristicDomainRepository mockCharacteristicRepository = mock(CharacteristicDomainRepository.class);
+        IRequirementsByTypeAndNameCharacteristic mockIRequirementsByTypeAndNameCharacteristic = mock(IRequirementsByTypeAndNameCharacteristic.class);
+        Requirement requirement = new Requirement();
+        requirement.setRequirementId(1);
+        requirement.setProjectId(1);
+        requirement.setName("Requisito ensayo");
+        requirement.setDescription("Requisito ensayo prueba");
+        requirement.setTypeRequirement("Funcional");
+
+        when(mockCharacteristicRepository.countRequirementsByTypeAndNameCharacteristic(any(), any())).thenReturn(mockIRequirementsByTypeAndNameCharacteristic);
+
+        //Act
+        IRequirementsByTypeAndNameCharacteristic result = mockCharacteristicRepository.countRequirementsByTypeAndNameCharacteristic(requirement.getTypeRequirement(), requirement.getProjectId());
+        //Assert
+        Mockito.verify(mockCharacteristicRepository, Mockito.times(1)).countRequirementsByTypeAndNameCharacteristic(requirement.getTypeRequirement(), requirement.getProjectId());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCountRequirementsByRequirementIdAndCauseError(){
+        //Arrange
+        CharacteristicDomainRepository mockCharacteristicRepository = mock(CharacteristicDomainRepository.class);
+        IRequirementsByRequirementIdAndCauseError mockIRequirementsByRequirementIdAndCauseError = mock(IRequirementsByRequirementIdAndCauseError.class);
+        Requirement requirement = new Requirement();
+        requirement.setRequirementId(1);
+        requirement.setProjectId(1);
+        requirement.setName("Requisito ensayo");
+        requirement.setDescription("Requisito ensayo prueba");
+        requirement.setTypeRequirement("Funcional");
+
+        when(mockCharacteristicRepository.countRequirementsByRequirementIdAndCauseError(any(), any())).thenReturn(mockIRequirementsByRequirementIdAndCauseError);
+
+        //Act
+        IRequirementsByRequirementIdAndCauseError result = mockCharacteristicRepository.countRequirementsByRequirementIdAndCauseError(requirement.getRequirementId(), requirement.getProjectId());
+        //Assert
+        Mockito.verify(mockCharacteristicRepository, Mockito.times(1)).countRequirementsByRequirementIdAndCauseError(requirement.getRequirementId(), requirement.getProjectId());
+        assertNotNull(result);
+    }
+    @Test
+    public void testCountCauseErrorByRequirementType(){
+        //Arrange
+        CharacteristicDomainRepository mockCharacteristicRepository = mock(CharacteristicDomainRepository.class);
+        IRequirementsByFilterCauseError mockIRequirementsByFilterCauseError = mock(IRequirementsByFilterCauseError.class);
+        Requirement requirement = new Requirement();
+        requirement.setRequirementId(1);
+        requirement.setProjectId(1);
+        requirement.setName("Requisito ensayo");
+        requirement.setDescription("Requisito ensayo prueba");
+        requirement.setTypeRequirement("Funcional");
+
+        when(mockCharacteristicRepository.countCauseErrorByRequirementType(any(), any())).thenReturn(mockIRequirementsByFilterCauseError);
+
+        //Act
+        IRequirementsByFilterCauseError result = mockCharacteristicRepository.countCauseErrorByRequirementType(requirement.getTypeRequirement(), requirement.getProjectId());
+
+        //Assert
+        Mockito.verify(mockCharacteristicRepository, Mockito.times(1)).countCauseErrorByRequirementType(requirement.getTypeRequirement(), requirement.getProjectId());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testCountRequirementsByTypeAndCauseError(){
+        //Arrange
+        CharacteristicDomainRepository mockCharacteristicRepository = mock(CharacteristicDomainRepository.class);
+        IRequirementsByTypeAndCauseError mockIRequirementsByTypeAndCauseError = mock(IRequirementsByTypeAndCauseError.class);
+        Requirement requirement = new Requirement();
+        requirement.setRequirementId(1);
+        requirement.setProjectId(1);
+        requirement.setName("Requisito ensayo");
+        requirement.setDescription("Requisito ensayo prueba");
+        requirement.setTypeRequirement("Funcional");
+
+        when(mockCharacteristicRepository.countRequirementsByTypeAndCauseError(any(), any())).thenReturn(mockIRequirementsByTypeAndCauseError);
+
+        //Act
+        IRequirementsByTypeAndCauseError result = mockCharacteristicRepository.countRequirementsByTypeAndCauseError(requirement.getTypeRequirement(), requirement.getProjectId());
+
+        //Assert
+        Mockito.verify(mockCharacteristicRepository, Mockito.times(1)).countRequirementsByTypeAndCauseError(requirement.getTypeRequirement(), requirement.getProjectId());
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testLevelWeightScoreForNineCharacters(){
+        //Arrange
+        CharacteristicDomainRepository mockCharacteristicRepository = mock(CharacteristicDomainRepository.class);
+        CharacteristicService mockCharacteristicService = mock(CharacteristicService.class);
+        Requirement requirement = new Requirement();
+        requirement.setRequirementId(1);
+        requirement.setProjectId(1);
+        requirement.setName("Requisito ensayo");
+        requirement.setDescription("Requisito ensayo prueba");
+        requirement.setTypeRequirement("Funcional");
+        Double maxScore= 81.0;
+
+        when(mockCharacteristicService.maximumAccumulatedScore(requirement.getRequirementId())).thenReturn(maxScore);
+
+        //Act
+        double result = mockCharacteristicService.levelWeightScoreForNineCharacters(requirement.getRequirementId());
+
+        //Assert
+        Mockito.verify(mockCharacteristicService, Mockito.times(1)).levelWeightScoreForNineCharacters(requirement.getRequirementId());
+        assertNotNull(result);
+    }
+
 
 
 }
---
+++ src/test/java/co/com/crud/requirement/domain/service/OperationServiceTest.java
@@ -0,0 +1,78 @@
+package co.com.crud.requirement.domain.service;
+
+import co.com.crud.requirement.domain.model.Operation;
+import co.com.crud.requirement.domain.repository.CharacteristicDomainRepository;
+import co.com.crud.requirement.domain.repository.OperationDomainRepository;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class OperationServiceTest {
+
+    @MockBean
+    OperationDomainRepository operationDomainRepository;
+
+    @Test
+    public void testGetAllOperationsWhenResultIsNotNull() {
+        //Arrange
+        OperationDomainRepository mockOperationDomainRepository = mock(OperationDomainRepository.class);
+        CharacteristicService characteristicService = mock(CharacteristicService.class);
+        Operation operation = new Operation();
+        operation.setOperationId(1);
+        operation.setRequirementId(1);
+        operation.setLevelAdecuacy(5.0);
+        operation.setEvaluatedCharacteristics(9.0);
+        operation.setLevelWeightScore(55.555);
+        operation.setMaximumScore(45.0);
+        operation.setCalculatedWeightAverage(0.555556);
+
+        ArrayList<Operation> mockOperations = new ArrayList<Operation>();
+        mockOperations.add(operation);
+        when(mockOperationDomainRepository.getAllOperations()).thenReturn(mockOperations);
+        OperationService operationService = new OperationService(mockOperationDomainRepository,characteristicService);
+
+        //Act
+        List<Operation> result = operationService.getAllOperations();
+
+        //Assert
+        verify(mockOperationDomainRepository, times(1)).getAllOperations();
+        assertNotNull(result);
+
+    }
+
+    @Test
+    public void testSaveOperation() {
+        //Arrange
+        OperationDomainRepository operationDomainRepository = mock(OperationDomainRepository.class);
+        CharacteristicService characteristicService = mock(CharacteristicService.class);
+        OperationService operationService = new OperationService(operationDomainRepository,characteristicService);
+        Operation operation = new Operation();
+        operation.setOperationId(1);
+        operation.setRequirementId(1);
+        operation.setLevelAdecuacy(5.0);
+        operation.setEvaluatedCharacteristics(9.0);
+        operation.setLevelWeightScore(55.555);
+        operation.setMaximumScore(45.0);
+        operation.setCalculatedWeightAverage(0.555556);
+
+        ArrayList<Operation> mockOperations = new ArrayList<Operation>();
+        mockOperations.add(operation);
+        when(operationDomainRepository.saveOperation(any())).thenReturn(operation);
+        when(characteristicService.allOperations(any(),any())).thenReturn(operation);
+        //Act
+        Operation result = operationService.saveOperation(operation);
+
+        //Assert
+        verify(operationDomainRepository, times(1)).saveOperation(operation);
+        assertEquals(operation, result);
+    }
+}

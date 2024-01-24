57bb0c3602b7ea04e94a275310631a09bd351e3e
SantiagoOspinaO <santiago.m200@outlook.es>
2024-01-23 22:39:36 -0500

    Se corrige valores de las constantes y puerto del frontend

--- src/main/java/co/com/crud/requirement/persistence/security/configuration/ProjectSecurityConfig.java
+++ src/main/java/co/com/crud/requirement/persistence/security/configuration/ProjectSecurityConfig.java
@@ -25,7 +25,7 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
                 cors().configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4201"));
                     config.setAllowedMethods(Collections.singletonList("*"));
                     config.setAllowCredentials(true);
                     config.setAllowedHeaders(Collections.singletonList("*"));
--- src/main/java/co/com/crud/requirement/web/constants/Constants.java
+++ src/main/java/co/com/crud/requirement/web/constants/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 
     public static final double LOWER_RANGE_WEIGHT = 72.09;
 
-    public static final String FUNCIONAL_TYPE = "funcional";
+    public static final String FUNCIONAL_TYPE = "Funcional";
 
-    public static final String NO_FUNCIONAL_TYPE = "no funcional";
+    public static final String NO_FUNCIONAL_TYPE = "NO Funcional";
 
 }

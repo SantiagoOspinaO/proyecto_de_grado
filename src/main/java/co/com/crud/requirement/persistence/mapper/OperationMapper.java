808bbcd19f433e60eae658253e8f64cac2eed438
Manuel Echverri <manuelecheverrir@yahoo.com>
2024-01-29 21:47:19 -0500

    Correcciones sonar

--- src/main/java/co/com/crud/requirement/persistence/mapper/OperationMapper.java
+++ src/main/java/co/com/crud/requirement/persistence/mapper/OperationMapper.java
@@ -5,22 +5,20 @@ import co.com.crud.requirement.persistence.entity.OperationEntity;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OperationMapper {
 
-    @Mappings({
-            @Mapping(source = "id", target = "operationId"),
-            @Mapping(source = "requisitoId", target = "requirementId"),
-            @Mapping(source = "nivelAdecuacion", target = "levelAdequacy"),
-            @Mapping(source = "caracteristicasEvaluadas", target = "evaluatedCharacteristics"),
-            @Mapping(source = "nivelCaracteristicaPuntuacion", target = "levelWeightScore"),
-            @Mapping(source = "puntajeMaximo", target = "maximumScore"),
-            @Mapping(source = "promedioCalculado", target = "calculatedWeightAverage")
-    })
+
+    @Mapping(source = "id", target = "operationId")
+    @Mapping(source = "requisitoId", target = "requirementId")
+    @Mapping(source = "nivelAdecuacion", target = "levelAdequacy")
+    @Mapping(source = "caracteristicasEvaluadas", target = "evaluatedCharacteristics")
+    @Mapping(source = "nivelCaracteristicaPuntuacion", target = "levelWeightScore")
+    @Mapping(source = "puntajeMaximo", target = "maximumScore")
+    @Mapping(source = "promedioCalculado", target = "calculatedWeightAverage")
     Operation toOperation(OperationEntity operationEntity);
 
     List<Operation> toOperationEntity(List<OperationEntity> operations);
--- src/main/java/co/com/crud/requirement/persistence/mapper/TypeErrorCharacteristicMapper.java
+++ src/main/java/co/com/crud/requirement/persistence/mapper/TypeErrorCharacteristicMapper.java
@@ -5,17 +5,15 @@ import co.com.crud.requirement.persistence.entity.TypeErrorCharacteristicEntity;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TypeErrorCharacteristicMapper {
-    @Mappings({
-            @Mapping(source = "id", target = "typeErrorId"),
-            @Mapping(source = "nombre", target = "name"),
-            @Mapping(source = "descripcion", target = "description")
-    })
+
+    @Mapping(source = "id", target = "typeErrorId")
+    @Mapping(source = "nombre", target = "name")
+    @Mapping(source = "descripcion", target = "description")
     TypeErrorCharacteristic toTypeError(TypeErrorCharacteristicEntity typeErrorCharacteristicEntity);
 
     List<TypeErrorCharacteristic> toTypesErrorsEntities(List<TypeErrorCharacteristicEntity> typesErrors);
